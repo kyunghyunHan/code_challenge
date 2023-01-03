fa8a5f9c9d27a02a75452a2e7a1b82083c517662
kyunghyunHan <ekfqlwcjswl@naver.com>
2023-01-03 13:48:52 +0900

    ㅁㅇㅇ

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,13 @@
 mod calculate_the_median;
-
+//중앙값 계산
+//숫자 목록의 계산
+//홀수개의 요소가 있다.
 fn main() {
-    calculate_the_median::test();
-    println!("Hello, world!");
+    let answer = median(vec![1.0, 2.0, 5.0]);
+
+    println!("median([1,2,5])={:?}", answer)
+}
+fn median(a: Vec<f32>) -> Option<f32> {
+    //todo!메인코드 동작은 프로그램을 충돌
+    todo!();
 }
